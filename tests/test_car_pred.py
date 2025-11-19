d07a80bdd43a95d25671dcd75bb0096d505aeb11
venkateshsureshcks <155462269+venkateshsureshcks@users.noreply.github.com>
2025-11-19 16:07:57 +0530

    Update test_car_pred.py

--- tests/test_car_pred.py
+++ tests/test_car_pred.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add project root to PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import json
 from Prediction.Car_pred import app
 
